9e644e4c3d125e08334526d5a333b18aa5a323cd
EloyHz <eloyhz@gmail.com>
2021-11-26 14:32:08 -0600

    ÂDay 3 finished

--- d03.c
+++ d03.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-#define MAXROW 20
-#define MAXCOL 20
+#define MAXROW 400
+#define MAXCOL 40
+
+char grid[MAXROW][MAXCOL];
+int count = 0;
+
+long long calculate(int right, int down);
 
 int main(void)  {
-    char grid[MAXROW][MAXCOL];
-    int count = 0;
+    freopen("d03.in", "r", stdin);
     while (scanf("%s", grid[count++]) != EOF) {
         // keep reading
     }
-    for (int i = 0; i < count; i++) {
-        for (const char *c = grid[i]; *c != '\0'; c++)
-            printf("(%c) ", *c);
-        printf("\n");
-    }
+    long long ans = calculate(1, 1);
+    ans *= calculate(3, 1);
+    ans *= calculate(5, 1);
+    ans *= calculate(7, 1);
+    ans *= calculate(1, 2);
+    printf("%lld\n", ans);
     return 0;
+}
+
+long long calculate(int right, int down) {
+    long long ans = 0;
+    int n = strlen(grid[0]);
+    for (int i = 0, j = 0; i < count; i += down) {
+        if (grid[i][j] == '#')
+            ans++;
+        j = (j + right) % n;
+    }
+    return ans;
 }
