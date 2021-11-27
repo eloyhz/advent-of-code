e4ec6a79b186cdf2453334f4ea032941d69fd0b1
EloyHz <eloyhz@gmail.com>
2021-11-27 11:57:47 -0600

    Advent of Code 2020 - Day 4 Part 2

--- d04.cpp
+++ d04.cpp
@@ -1,15 +1,82 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <map>
 #include <set>
-#include <vector>
+#include <cctype>
 
 using namespace std;
 
-bool is_valid(set<string> &fields) {
-    for (string s : {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"})  {
-        if (fields.count(s) == 0) {
+bool is_valid_year(string year_string, int least, int most)    {
+    try {
+        int year = stoi(year_string);
+        if (year < least || year > most) {
             return false;
+        }
+    }   catch(const std::invalid_argument& e)   {
+        return false;
+    }
+    return true;    
+}
+
+bool is_valid_passport(map<string, string> &fields) {
+    for (string key : {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"})  {
+        if (fields.count(key) == 0) {
+            return false;
+        }
+        if (key == "byr" && !is_valid_year(fields[key], 1920, 2002))   {
+            return false;
+        }
+        else if (key == "iyr" && !is_valid_year(fields[key], 2010, 2020))   {
+            return false;
+        }
+        else if (key == "eyr" && !is_valid_year(fields[key], 2020, 2030))   {
+            return false;
+        }
+        else if (key == "hgt")    {
+            int value;
+            try {
+                value = stoi(fields[key].substr(0, fields[key].length()-2));
+            }   catch(const std::invalid_argument& e)   {
+                return false;
+            }
+            string units = fields[key].substr(fields[key].length()-2);
+            if (units == "cm" && (value < 150 || value > 193))  {
+                return false;
+            }
+            else if (units == "in" && (value < 59 || value > 76))   {
+                return false;
+            }
+            else if (units != "in" && units != "cm")    {
+                return false;
+            }
+        }
+        else if (key == "hcl")  {
+            string value = fields[key];
+            if (value.length() != 7 || value[0] != '#') {
+                return false;
+            }
+            for (int i = 1; i <= 6; i++)
+                if (!isxdigit(value[i]))    {
+                    return false;
+                }
+        }
+        else if (key == "ecl")  {
+            string value = fields[key];
+            set<string> s = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+            if (s.count(value) == 0)    {
+                return false;
+            }
+        }
+        else if (key == "pid")  {
+            if (fields[key].length() != 9)  {
+                return false;
+            }
+            for (char c : fields[key])  {
+                if (!isdigit(c))    {
+                    return false;
+                }
+            }
         }
     }
     return true;
@@ -18,10 +85,10 @@ bool is_valid(set<string> &fields) {
 int main()  {
     string line;
     int count = 0;
-    set<string> fields;
+    map<string, string> fields;
     while (getline(cin, line)) {
         if (line == "") {
-            if (is_valid(fields))  {
+            if (is_valid_passport(fields))  {
                 count++;
             }
             fields.clear();
@@ -32,11 +99,12 @@ int main()  {
             string passport;
             while (getline(ss, passport, ' ')) {
                 string key = passport.substr(0, 3);
-                fields.insert(key);
+                string value = passport.substr(4);
+                fields[key] = value;
             }
         }
     }
-    if (is_valid(fields))  {
+    if (is_valid_passport(fields))  {
         count++;
     }
 
