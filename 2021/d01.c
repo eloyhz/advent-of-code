a11d2dcc2eca72b52879cec860cc0fdd9d5deadc
Eloy Hernandez <eloyhz@gmail.com>
2021-12-01 20:09:51 -0600

    AoC2021 - Day 1 - Part 2 solved

--- 2021/d01.c
+++ 2021/d01.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+#define MAX 2000
+
 int main(void)  {
-    int prev, current;
+    int prev1, prev2, current;
+    int prev_sum, current_sum;
     int count = 0;
 
-    scanf("%d", &prev);
+    freopen("d01.in", "r", stdin);
+
+    scanf("%d", &prev1);
+    scanf("%d", &prev2);
+    scanf("%d", &current);
+    prev_sum = current + prev2 + prev1;
+    prev1 = prev2;
+    prev2 = current;
     while (scanf("%d", &current) == 1)  {
-        if (current > prev)
+        current_sum = current + prev2 + prev1;
+        if (current_sum > prev_sum)
             count++;
-        prev = current;
+        prev_sum = current_sum;
+        prev1 = prev2;
+        prev2 = current;
     }
     printf("%d\n", count);
 }
